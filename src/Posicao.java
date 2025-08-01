7703632ae64e584c4647ac90c3c9e554eed7811e
juliacazeri <juliaczanatta@gmail.com>
2025-08-01 14:21:09 -0300

    Adicionando Posicao.java e Tabuleiro.java

---
+++ src/Posicao.java
@@ -0,0 +1,19 @@
+public class Posicao{
+    private int linha;
+    private int coluna;
+
+    public Posicao(int linha, int coluna){
+        if(linha < 0 || linha >= 9 || coluna < 0 || coluna >= 9){
+            throw new IllegalArgumentException("A posição está fora dos limites: " + (linha + 1) + "," + (coluna + 1) + ".");
+        }
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getLinha(){
+        return linha;
+    }
+    public int getColuna(){
+        return coluna;
+    }
+}
---
+++ src/Tabuleiro.java
@@ -0,0 +1,44 @@
+public class Tabuleiro{
+    private final int tamanho = 9;
+    private int[][] numeros;
+    private boolean[][] numeroFixo;
+
+    public Tabuleiro(){
+        numeros = new int[tamanho][tamanho];
+        numeroFixo = new boolean[tamanho][tamanho];
+    }
+
+    public int getNumeros(Posicao pos){
+        return numeros[pos.getLinha()][pos.getColuna()];
+    }
+
+    public void setNumeros(Posicao pos, int numero, boolean ehFixo){
+        numeros[pos.getLinha()][pos.getColuna()] = numero;
+
+        if(ehFixo){
+            numeroFixo[pos.getLinha()][pos.getColuna()] = true;
+        }
+    }
+
+    public int getTamanho(){
+        return tamanho;
+    }
+
+    public boolean ehFixo(Posicao pos){
+        return numeroFixo[pos.getLinha()][pos.getColuna()];
+    }
+
+    public void limparNumero(Posicao pos){
+        if (!numeroFixo[pos.getLinha()][pos.getColuna()]){
+            numeros[pos.getLinha()][pos.getColuna()] = 0;
+        }
+    }
+
+    public void limparNumeros(){
+        for(int l = 0; l < tamanho; l++){
+            for(int c = 0; c < tamanho; c++){
+                if(!numeroFixo[l][c]) numeros[l][c] = 0;
+            }
+        }
+    }
+}
